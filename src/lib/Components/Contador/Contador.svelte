6dc6f9626a0ccf6683d3c2e73771cebef1fa6024
Andy Uzal <andyuzal@gmail.com>
2024-10-29 15:04:00 +0100

    feat: componentes de Andy

---
+++ src/lib/Components/Contador/Contador.svelte
@@ -0,0 +1,28 @@
+<script>
+
+   import "./contador.css"
+   let contador = 0
+  
+  const sumar = () => {
+    contador+=1;
+  }
+
+  const restar = () => {
+    if( contador <= 0){
+        contador = 0;
+        return;
+    }
+    contador-=1;
+  }
+
+  const resetear = () => {
+    contador = 0;
+  }
+
+</script>
+
+<button on:click={sumar}>+</button>
+<button on:click={restar}>-</button>
+<button on:click={resetear}>Resetear</button>
+
+<p>{contador}</p>
---
+++ src/lib/Components/Header/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "./header.css"
+    export let titulo = ""
+    export let color = "red"
+    export let tam = 0
+</script> 
+
+<p
+style="background-color: {color}; font-size: {tam}rem;"
+class="titulo-header">
+    {titulo}
+</p>
