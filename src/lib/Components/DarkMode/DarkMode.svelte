1c74447ab9c4c143225f31739a9150d585ac0453
Andy Uzal <andyuzal@gmail.com>
2024-11-05 13:03:46 +0100

    fix: Agregando componentes de la rama Andy

---
+++ src/lib/Components/DarkMode/DarkMode.svelte
@@ -0,0 +1,21 @@
+<script>
+   import "./darkmode.css"
+   let toogle = false;
+   const toggleDarkMode = () => {
+       toogle = !toogle;
+   }
+</script>
+
+<button on:click={toggleDarkMode}>
+   {toogle ? "Light Mode" : "Dark Mode"}
+</button>
+
+{#if toogle}
+<div class="dark-mode-container">
+    <p>Estamos en modo oscuro</p>
+</div>
+{:else}
+<div class="light-mode-container">
+    <p>Estamos en modo claro</p>
+</div>
+{/if}  
---
+++ src/lib/Components/Galeria/GaleriaModal.svelte
@@ -0,0 +1,15 @@
+<script >
+    import "./galeriaModal.css"
+   import imagen from "../../../assets/svelte.svg"
+</script>
+
+<div class="galeria-container">
+    <div> 
+        <img src={imagen} alt="">
+        <img src={imagen} alt="">
+    </div>
+    <div>
+        <img src={imagen} alt="">
+    </div>
+</div>
+    
