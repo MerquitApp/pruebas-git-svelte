b7243660f7a9e2771e4e20a7d3377c2054414f49
Andy Uzal <andyuzal@gmail.com>
2024-11-05 12:12:27 +0100

    fix: Agregando estructura de footer

---
+++ src/lib/Components/Footer/Footer.svelte
@@ -0,0 +1,14 @@
+<script >
+    import "./Footer.css";
+   
+</script>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            Agregando la estructura del footer
+
+        </div>
+        
+    </div>
+</footer>
